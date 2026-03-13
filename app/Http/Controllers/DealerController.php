f8b47c0ad6b905cca945462754cbe70b341547f6
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-03-13 10:19:05 +0100

    kész feladat

--- app/Http/Controllers/DealerController.php
+++ app/Http/Controllers/DealerController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dealer;
+use Exception;
 use Illuminate\Http\Request;
+use Throwable;
 
 class DealerController extends Controller
 {
@@ -11,7 +14,8 @@ class DealerController extends Controller
      */
     public function index()
     {
-        
+        $dealers = Dealer::with("table")->get();
+        return response()->json($dealers, 200, options:JSON_UNESCAPED_UNICODE);
     }
 
     /**
@@ -19,7 +23,36 @@ class DealerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string',
+            'gender' => 'required|in:ferfi,no',
+            'online' => 'required|boolean',
+            'table_id' => 'required|exists:tables,id'
+        ],[
+            'required' => 'A :attribute mezo kitoltese kötelező',
+            'string' => 'A :attribute stringnek kell lennie',
+            'in' => 'A :attribute mezőnek férfinek vagy nőnek kell lennie',
+            'boolean' => 'A :attribute mezőnek booleannek kell lennie',
+            'exists' => 'A :attribute mezőnek léteznie kell'
+        ],[
+            'name' => 'név',
+            'gender' => 'nem',
+            "online" => "onlájn",
+            'table_id' => 'tábla Id'
+        ]);
+
+        try {
+            Dealer::create([
+                'name' => $request->name,
+                'gender' => $request->gender,
+                'online' => $request->online,
+                'table_id' => $request->table_id
+            ]);
+    
+            return response()->json(['uzenet' => 'Sikeresen rögzítve.'], 201, options:JSON_UNESCAPED_UNICODE);
+        } catch (Throwable $ex) {
+            return response()->json(['uzenet' => 'Meghiúsult az adat létrehozása.'], 500, options:JSON_UNESCAPED_UNICODE);
+        }
     }
 
     /**
@@ -35,7 +68,36 @@ class DealerController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $dealer = Dealer::find($id);
+
+        if (!$dealer) {
+            return response()->json(["uzenet" => "Nincs dealer ezzel az id-val"], 404, options:JSON_UNESCAPED_UNICODE);
+        }
+
+        $request->validate([
+            'online' => 'sometimes|required|boolean',
+            'table_id' => 'sometimes|required|exists:tables,id'
+        ],[
+            'required' => 'A :attribute mezo kitoltese kötelező',
+            'boolean' => 'A :attribute mezőnek booleannek kell lennie',
+            'exists' => 'A :attribute mezőnek léteznie kell'
+        ],[
+            "table_id" => "tábla Id",
+            "online" => "onlájn"
+        ]);
+        
+        if($request->has("online"))
+        {
+            $dealer->online = $request->online;
+        };
+        if($request->has("table_id"))
+        {
+            $dealer->table_id = $request->table_id;
+        };
+
+        $dealer->save();
+
+        return response()->json(["uzenet" => "A módosítás sikeres"], 200, options:JSON_UNESCAPED_UNICODE);
     }
 
     /**
